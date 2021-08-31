0c76066a4d3948198e6310951978019637a49c58
mcieciora <michalcieciora96@gmail.com>
2021-08-31 20:36:17 +0100

    31082021-2036

--- automated_tests/units/test_auth_login.py
+++ automated_tests/units/test_auth_login.py
@@ -1,47 +1,50 @@
-import pytest
-from src.website import create_app
+def test_auth_login__no_such_username(app_empty):
+    response = app_empty.post("/login", data=dict(username="user1", pin_code="1234"), follow_redirects=True)
+    assert response.status_code == 200, f'Expected response status code: 200, actual: {response.status_code}'
+    assert '<strong>Warning!</strong> Username does not exist.' in response.data.decode(), \
+        f'Application shall not login user that does not exist\n{response.data}'
 
 
-@pytest.fixture
-def client():
-    app = create_app()
-    app.config['TESTING'] = True
-
-    with app.app_context():
-        with app.test_client() as client:
-            yield client
+def test_auth_login__wrong_pin_code(app_basic):
+    response = app_basic.post("/login", data=dict(username="user1", pin_code="1234"), follow_redirects=True)
+    assert response.status_code == 200, f'Expected response status code: 200, actual: {response.status_code}'
+    assert '<strong>Warning!</strong> Incorrect pin code, try again.' in response.data.decode(), \
+        f'Application shall not login user who provided wrong credentials\n{response.data}'
 
 
-@pytest.mark.skip('To be implemented')
-def test_auth_login__no_such_username(client):
-    pass
+def test_auth_login__empty_login(app_basic):
+    response = app_basic.post("/login", data=dict(username="", pin_code="1234"), follow_redirects=True)
+    assert response.status_code == 200, f'Expected response status code: 200, actual: {response.status_code}'
+    assert '<strong>Warning!</strong> Username does not exist.' in response.data.decode(), \
+        f'Application shall not login user that does not exist\n{response.data}'
 
 
-@pytest.mark.skip('To be implemented')
-def test_auth_login__wrong_password(client):
-    pass
+def test_auth_login__empty_pin_code(app_basic):
+    response = app_basic.post("/login", data=dict(username="user1", pin_code=""), follow_redirects=True)
+    assert response.status_code == 200, f'Expected response status code: 200, actual: {response.status_code}'
+    assert '<strong>Warning!</strong> Incorrect pin code, try again.' in response.data.decode(), \
+        f'Application shall not login user who provided wrong credentials\n{response.data}'
 
 
-@pytest.mark.skip('To be implemented')
-def test_auth_login__empty_login(client):
-    pass
+def test_auth_login__empty_credentials(app_basic):
+    response = app_basic.post("/login", data=dict(username="", pin_code=""), follow_redirects=True)
+    assert response.status_code == 200, f'Expected response status code: 200, actual: {response.status_code}'
+    assert '<strong>Warning!</strong> Username does not exist.' in response.data.decode(), \
+        f'Application shall not login user that does not exist\n{response.data}'
 
 
-@pytest.mark.skip('To be implemented')
-def test_auth_login__empty_password(client):
-    pass
+def test_auth_login__correct_credentials(app_basic):
+    response = app_basic.post("/login", data=dict(username="user1", pin_code="1234"), follow_redirects=True)
+    assert response.status_code == 200, f'Expected response status code: 200, actual: {response.status_code}'
+    assert '<strong>Warning!</strong> Username does not exist.' in response.data.decode(), \
+        f'Application shall not login user that does not exist\n{response.data}'
 
 
-@pytest.mark.skip('To be implemented')
-def test_auth_login__empty_credentials(client):
-    pass
-
-
-@pytest.mark.skip('To be implemented')
-def test_auth_login__correct_credentials(client):
-    pass
-
-
-@pytest.mark.skip('To be implemented')
-def test_auth_logout__logout(client):
-    pass
+def test_auth_logout__logout(app_basic):
+    response = app_basic.post("/login", data=dict(username="user1", pin_code="1234"), follow_redirects=True)
+    assert response.status_code == 200, f'Expected response status code: 200, actual: {response.status_code}'
+    assert '<strong>Warning!</strong> Username does not exist.' in response.data.decode(), \
+        f'Application shall not login user that does not exist\n{response.data}'
+    response = app_basic.post("/logout", follow_redirects=True)
+    assert '<strong>Warning!</strong> Username does not exist.' in response.data.decode(), \
+        f'Application shall not login user that does not exist\n{response.data}'
